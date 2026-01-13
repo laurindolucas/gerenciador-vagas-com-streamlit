cac69477330cd58632c128757728238cd196a32a
laurindolucas <caiolucax2024@gmail.com>
2026-01-12 21:48:14 -0300

    feat: adicionandoi estrutura base da tela de cadastro

--- my_app/pages/02_Cadastros.py
+++ my_app/pages/02_Cadastros.py
@@ -1,3 +1,18 @@
 import streamlit as st 
 from db.connection import SessionLocal 
 from db.models import Vaga, Estacionamento
+
+
+st.title("Primeiros Cadastros")
+st.write("Faça o primeiro cadastro de veiculos aqui")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    
+    
+    
+    
+    
+with col2:  
+    
