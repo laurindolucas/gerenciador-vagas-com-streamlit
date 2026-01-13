1f22511d46c399d35b0892a08f4af6e9fce2305e
laurindolucas <caiolucax2024@gmail.com>
2026-01-12 22:12:10 -0300

    feat: adicionando form de entrada de dados do primeiro cadastro

--- my_app/pages/02_Cadastros.py
+++ my_app/pages/02_Cadastros.py
@@ -1,7 +1,7 @@
 import streamlit as st 
 from db.connection import SessionLocal 
 from db.models import Vaga, Estacionamento
-
+import pandas as pd
 
 st.title("Primeiros Cadastros")
 st.write("Faça o primeiro cadastro de veiculos aqui")
@@ -9,10 +9,38 @@ st.write("Faça o primeiro cadastro de veiculos aqui")
 col1, col2 = st.columns(2)
 
 with col1:
+    proprietario_carro= st.text_input(
+        "Digite o nome do proprietario do carro:",
+        placeholder="ex: Caleb Araujo..."   
+    )
+     
+    placa_carro= st.text_input(
+        "Digite a placa do carro:",
+        placeholder="ex: DEV 1399..."   
+    )
+    
+    cor_carro= st.text_input(
+        "Digite a cor do carro:",
+        placeholder="ex: Vermelho..."   
+    )
     
     
+with col2:
+    numero_proprietario= st.text_input(
+        "Digite o número do proprietario do carro:",
+        placeholder="ex: (81) 9 98855-3344..."   
+    )
+    tipo_Veiculo= st.selectbox(
+        "Selecione o tipo do veiculo?", 
+        ("Carro", "Moto", "Caminhão", "Ônibus", "Outro") 
+        )
     
+    modelo_carro= st.text_input(
+        "Digite o modelo do carro:",
+        placeholder="ex: Civic..."   
+    )
     
-    
-with col2:  
+confirmarButton= st.container(horizontal=True, horizontal_alignment="center")
+
+botao = confirmarButton.button("Cadastar")
     
