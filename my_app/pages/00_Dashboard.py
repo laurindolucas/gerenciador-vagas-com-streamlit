745638717c024b6097ca0fcf50147c803d434cef
laurindolucas <caiolucax2024@gmail.com>
2026-02-01 18:49:39 -0300

    commit para sicronizar aparelhos

--- my_app/pages/00_Dashboard.py
+++ my_app/pages/00_Dashboard.py
@@ -1,5 +1,10 @@
 import streamlit as st
 
+if "usuario_id" not in st.session_state:
+    st.error("Faça login para acessar esta página")
+    st.stop()
+    
+    
 st.set_page_config(
     page_title="Cadastro Page"
 )
--- my_app/pages/03_Relatótrios.py
+++ my_app/pages/03_Relatótrios.py
@@ -1,4 +1,7 @@
 import streamlit as st 
 from db.connection import SessionLocal 
-from db.models import Vaga, Estaci0onamento
+from db.models import *
 
+if "usuario_id" not in st.session_state:
+    st.error("Faça login para acessar esta página")
+    st.stop()
